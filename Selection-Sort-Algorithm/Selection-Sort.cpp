394d6ef6c6c6f4f3bc9d81cee4462650bdada3b9
Shattajit <jeet.shatta@gmail.com>
2025-03-11 18:23:09 +0600

    Sorting Algorithms

---
+++ Selection-Sort-Algorithm/Selection-Sort.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(int tc){
+    cout<<"Hello\n";
+}
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc = 1;
+
+    cin>>tc;
+
+    for(int i=1;i<=tc;i++){
+        solve(tc);
+    }
+}
