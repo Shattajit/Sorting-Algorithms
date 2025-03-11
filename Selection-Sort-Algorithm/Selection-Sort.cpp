1fcef657dcd351a5eb609601a4495bdbbc3d6e46
Shattajit <jeet.shatta@gmail.com>
2025-03-11 18:54:33 +0600

    Selection Sort

--- Selection-Sort-Algorithm/Selection-Sort.cpp
+++ Selection-Sort-Algorithm/Selection-Sort.cpp
@@ -1,8 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int find_min(int *ar, int idx, int n){
+
+    for(int i=idx+1;i<n;i++){  // it should be idx + 1 becuase the idx position will be filled with the smallest element from the range in {idx, n}..
+
+        if(ar[i] < ar[idx]){
+            idx = i;
+        }
+    }
+
+    return idx;
+}
 void solve(int tc){
-    cout<<"Hello selection\n";
+
+    int ar[8] = {2, 1, 6, 2, 3, 1, 4, 5};
+
+    for(int i=0;i<8-1;i++){
+
+        int idx = find_min(ar, i, 8);
+
+        swap(ar[i], ar[idx]);
+    }
+
+    for(int i=0;i<8;i++) cout<<ar[i]<<" \n"[i==7];
 }
 int main(){
 
@@ -11,7 +32,7 @@ int main(){
 
     int tc = 1;
 
-    cin>>tc;
+    //cin>>tc;
 
     for(int i=1;i<=tc;i++){
         solve(tc);
